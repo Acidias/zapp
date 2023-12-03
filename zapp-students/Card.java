1599d73cd23ba4cd7c56eb572d8435090dcbecb8
Mihaly Arpad Dani <dmisi98@gmail.com>
2023-12-03 17:23:02 +0000

    Test submit -MD

--- zapp-students/Card.java
+++ zapp-students/Card.java
@@ -1,16 +1,12 @@
- 
 
-   
 /**
  * A Card has an id number, name, a luxury rating, number of credits and points.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Card 
-{
+public class Card {
 
-    
-    
+     // Test git submit
+
 }
-
